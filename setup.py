9640f39ae891081ef24c1a5aae649fafcbe4923b
Alexander Sosedkin <monk@unboiled.info>
2018-04-23 04:07:18 +0700

    Unlock colorama version as per discussion in #12

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     py_modules=['backtrace'],
     entry_points={'console_scripts': ['backtrace = backtrace:main']},
     install_requires=[
-        "colorama==0.3.7",
+        "colorama>=0.3.7",
     ],
     classifiers=[
         'Programming Language :: Python',
